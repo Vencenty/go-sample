c34ee3da7367f65627ff72dbb4f384e546f32e1a
yanchengtian <yanchengtian@popmart.com>
2024-02-01 11:18:21 +0800

    增加Stack数据结构

--- algo/Stack/main.go
+++ algo/Stack/main.go
@@ -1,86 +1,36 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-type Tree struct {
-	Children map[string]*Tree
-	IsEnd    bool
+type Stack struct {
+	Elements []interface{}
 }
 
-// /v1/user/callback
-func (t *Tree) Insert(word string) {
-	paths := strings.Split(word, "/")
-	node := t
-	for _, path := range paths {
-		if path == "" {
-			continue
-		}
-		if _, ok := node.Children[path]; !ok {
-			node.Children[path] = &Tree{
-				Children: make(map[string]*Tree),
-			}
-		}
-		node = node.Children[path]
+func (s *Stack) Add(element ...interface{}) {
+	s.Elements = append(s.Elements, element...)
+}
+
+func (s *Stack) Pop() interface{} {
+	if len(s.Elements) == 0 {
+		return nil
 	}
-	node.IsEnd = true
+	// 最后一个元素的索引
+	lastIndex := len(s.Elements) - 1
+	button := s.Elements[lastIndex]
+	s.Elements = s.Elements[:lastIndex]
+	return button
 }
 
-func (t *Tree) Search(word string) bool {
-	paths := strings.Split(word, "/")
-	node := t
-	for _, path := range paths {
-		if path == "" {
-			continue
-		}
-		if searched, ok := node.Children[path]; ok {
-			node = searched
-		} else {
-			node.IsEnd = false
-		}
-	}
-	return node.IsEnd
-}
-
-func (t *Tree) StartWith(word string) bool {
-	paths := strings.Split(word, "/")
-	node := t
-	for _, path := range paths {
-		if path == "" {
-			continue
-		}
-		if searched, ok := node.Children[path]; ok {
-			node = searched
-		} else {
-			node.IsEnd = false
-		}
-	}
-	return node.IsEnd
-}
-
-func (t *Tree) PrintTree() {
-	for path, v := range t.Children {
-		if v.IsEnd {
-			fmt.Println("这是最后的路径了", path)
-		}
-		v.PrintTree()
+func (s *Stack) List() {
+	for _, v := range s.Elements {
+		fmt.Println(v)
 	}
 }
 
-// 后入先出
 func main() {
-	t := &Tree{Children: make(map[string]*Tree)}
-	t.Insert("/v1/auth/callback")
-	t.Insert("/v1/user/register")
-	t.Insert("/v1/user/get/name")
-	t.Insert("/v1/user/resetPassword")
-	t.Insert("/v2/user/resetPassword")
-	t.Insert("/v3/user/resetPassword")
-
-	r := t.Search("/v1/user/resetPassword")
-	fmt.Println(r)
-	t.PrintTree()
-
+	s := &Stack{}
+	s.Add(1, 2, 3)
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
+	fmt.Println(s.Pop())
 }
