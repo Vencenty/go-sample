a08a899d67978cb1b7edf13007203dccc4e65cb4
yanchengtian <yanchengtian@popmart.com>
2024-02-01 10:56:19 +0800

    增加队列实现

--- algo/Queue/main.go
+++ algo/Queue/main.go
@@ -1,1 +1,38 @@
-package Queue
+package main
+
+import "fmt"
+
+type Queue struct {
+	Elements []interface{}
+}
+
+func (q *Queue) Enqueue(elements ...interface{}) {
+	q.Elements = append(q.Elements, elements...)
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if len(q.Elements) == 0 {
+		return nil
+	}
+	element := q.Elements[0]
+	q.Elements = q.Elements[1:]
+	return element
+}
+
+func (q *Queue) List() {
+	for _, v := range q.Elements {
+		fmt.Println(v)
+	}
+}
+
+func main() {
+	q := new(Queue)
+	q.Enqueue(1, 2, 3)
+	q.Enqueue(4, 5, 6)
+	q.Enqueue(7, 8, 9)
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println(q.Dequeue())
+	fmt.Println("-------")
+	q.List()
+}
